7b182bbcf286825401af5d05ccbfe515c562be9a
Steven A. Zaluk <stevezaluk@gmail.com>
2024-12-08 19:04:18 -0500

    Added flags for each of our configuration options. Most of these will not be implemented until a later commit

--- cmd/inject.go
+++ cmd/inject.go
@@ -17,6 +17,7 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/spf13/viper"
 
 	"github.com/spf13/cobra"
 )
@@ -32,4 +33,25 @@ var injectCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(injectCmd)
+
+	injectCmd.Flags().StringP("input", "i", "", "The input path you want to search for protobufs with")
+	viper.BindPFlag("input", injectCmd.Flags().Lookup("input"))
+
+	injectCmd.Flags().StringP("file-ext", "f", ".pb.go", "The file extensions that should be considered for injection")
+	viper.BindPFlag("tag.file-ext", injectCmd.Flags().Lookup("file-ext"))
+
+	injectCmd.Flags().BoolP("recursive", "r", false, "Recurse into subdirectories of the input directory")
+	viper.BindPFlag("tag.recursive", injectCmd.Flags().Lookup("recursive"))
+
+	injectCmd.Flags().Bool("remove-comments", false, "Remove comments from generated protobufs")
+	viper.BindPFlag("tag.remove-comments", injectCmd.Flags().Lookup("remove-comments"))
+
+	injectCmd.Flags().String("comment-prefix", "@gotags", "The prefix of the comment that protoc-go-inject-tag should search for when looking for tags to inject")
+	viper.BindPFlag("tag.comment-prefix", injectCmd.Flags().Lookup("comment-prefix"))
+
+	injectCmd.Flags().StringSlice("skip", nil, "Tags that should be skipped")
+	viper.BindPFlag("tag.skip", injectCmd.Flags().Lookup("skip"))
+
+	injectCmd.Flags().StringSlice("tags", nil, "Additional tags that should be applied to all fields")
+	viper.BindPFlag("tag.additional-tags", injectCmd.Flags().Lookup("tags"))
 }
