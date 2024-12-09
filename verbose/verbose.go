0d53b20a16b5afff2a901fda5ef56844a91faf19
Steven A. Zaluk <stevezaluk@gmail.com>
2024-12-09 12:15:46 -0500

    Updated the logf function to check viper for verbosity

--- verbose/verbose.go
+++ verbose/verbose.go
@@ -1,14 +1,17 @@
 package verbose
 
 import (
+	"github.com/spf13/viper"
 	"log"
 )
 
-var verbose = false
-
+/*
+Logf soon to be deprecated. Slog logging will replace this function
+*/
 func Logf(format string, v ...interface{}) {
-	if !verbose {
+	if !viper.GetBool("verbose") {
 		return
 	}
+
 	log.Printf(format, v...)
 }
