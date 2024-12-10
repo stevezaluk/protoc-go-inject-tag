44ff85bb71889fb0dd0b455b748d5440948351ad
Steven A. Zaluk <stevezaluk@gmail.com>
2024-12-10 17:14:35 -0500

    Updated the inject sub command to use WalkDir

--- cmd/inject.go
+++ cmd/inject.go
@@ -37,7 +37,7 @@ var injectCmd = &cobra.Command{
 		}
 	},
 	Run: func(cmd *cobra.Command, args []string) {
-		protoFiles.IterFiles(viper.GetString("input"))
+		protoFiles.WalkDir(viper.GetString("input"))
 	},
 }
 
