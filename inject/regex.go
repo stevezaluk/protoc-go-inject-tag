285aafb043d8126ee032ddb59b93d3055a1fdd17
Steven A. Zaluk <stevezaluk@gmail.com>
2024-12-12 14:21:15 -0500

    Comments for regex functions

--- inject/regex.go
+++ inject/regex.go
@@ -6,6 +6,9 @@ import (
 	"regexp"
 )
 
+/*
+InitRegex Initialize regular expressions used for parsing comments pulled from the AST, and store them in viper
+*/
 func InitRegex() {
 	viper.Set("tag.regex.comment", regexp.MustCompile(fmt.Sprintf(`^//.*?@(?i:%s?):\s*(.*)$`, viper.GetString("tag.comment-prefix"))))
 	viper.Set("tag.regex.inject", regexp.MustCompile("`.+`$"))
@@ -13,6 +16,9 @@ func InitRegex() {
 	viper.Set("tag.regex.all", regexp.MustCompile(".*"))
 }
 
+/*
+GetRegex Fetch a value from viper and cast it as a Regexp pointer.
+*/
 func GetRegex(name string) *regexp.Regexp {
 	regex := viper.Get(name)
 
