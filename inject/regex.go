ee02b32e3015f46a4fd2d30009bb8e657dcb35f2
Steven A. Zaluk <stevezaluk@gmail.com>
2024-12-12 14:18:20 -0500

    Implemented support for intializing regular expressions and storing them within viper. GetRegex was implemented to ensure that we can always cast the value of viper.Get to a Regexp pointer

--- inject/regex.go
+++ inject/regex.go
@@ -1,3 +1,20 @@
 package inject
 
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"regexp"
+)
 
+func InitRegex() {
+	viper.Set("tag.regex.comment", regexp.MustCompile(fmt.Sprintf(`^//.*?@(?i:%s?):\s*(.*)$`, viper.GetString("tag.comment-prefix"))))
+	viper.Set("tag.regex.inject", regexp.MustCompile("`.+`$"))
+	viper.Set("tag.regex.tags", regexp.MustCompile(`[\w_]+:"[^"]+"`))
+	viper.Set("tag.regex.all", regexp.MustCompile(".*"))
+}
+
+func GetRegex(name string) *regexp.Regexp {
+	regex := viper.Get(name)
+
+	return regex.(*regexp.Regexp)
+}
